905e06a2b5dbd5af9af5a75f5b1725e9f661fa51
Carlos-Bernalte <carlos.bernalte@outlook.com>
2020-12-17 19:03:28 +0100

    Update IngredientDAO.java
    Added methods:
    -removeIngredients
    -updateIngredients
    -readAllIngredientsNames

--- RestaurantChainManagement/It3/src/main/java/A01/RestaurantChainManagement/persistance/IngredientDAO.java
+++ RestaurantChainManagement/It3/src/main/java/A01/RestaurantChainManagement/persistance/IngredientDAO.java
@@ -16,7 +16,7 @@ public class IngredientDAO {//Iteration2
 	/**********************Main functions**********************/
 	public void readAllAvailables() {
 		try {
-			Vector<Vector<Object>> auxVector = Broker.getAgente().leer("SELECT Name FROM A01dbservice.Ingredients WHERE Availability = 1;");
+			Vector<Vector<Object>> auxVector = Broker.getAgente().leer("SELECT Name FROM A01dbservice.Ingredients WHERE Stock > 0;");
 
 			for(int i=0; i<auxVector.size(); i++) {
 				getIngredientsList().add((String) auxVector.elementAt(i).elementAt(0));
@@ -27,6 +27,41 @@ public class IngredientDAO {//Iteration2
 		}
 	}
 	
+	public void removeIngredients(String[] ingredients) {
+		try {
+			for(int i=0; i<=ingredients.length;i++) {
+				Vector<Vector<Object>> stockVector=Broker.getAgente().leer("SELECT Stock FROM A01dbservice.Ingredients WHERE Name="+ingredients[i]+";");
+				int nuevoStock=((Integer) stockVector.elementAt(0).elementAt(0))-1;
+				Broker.getAgente().cambiar("UPDATE A01dbservice.Ingredients SET Stock="+nuevoStock+"WHERE Name=="+ingredients[i]+";");	
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+	}
+	public void updateIngredients(String[] ingredients, int[] quantity) {
+		try {
+			for(int i=0; i<=ingredients.length;i++) {
+				Broker.getAgente().cambiar("UPDATE A01dbservice.Ingredients SET Stock="+quantity[i]+"WHERE Name=="+ingredients[i]+";");	
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+	}
+	
+	public void readAllIngredientsNames() {
+		try {
+			Vector<Vector<Object>> auxVector = Broker.getAgente().leer("SELECT Name FROM A01dbservice.Ingredients;");
+
+			for(int i=0; i<auxVector.size(); i++) {
+				getIngredientsList().add((String) auxVector.elementAt(i).elementAt(0));
+			}
+			
+		}catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
 	/*******************Auxiliary functions********************/
 	public Vector<String> getIngredientsList() {
 		return this.IngredientsList;
